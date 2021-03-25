48f146d1c1516431bf966f29b257d4fcd0586f7a
Riddhigoyal <72336061+Riddhigoyal@users.noreply.github.com>
2021-03-25 18:00:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound ("coin.wav");
+	mario_over = loadSound ("gameover.wav");
+	mario_kick = loadSound ("kick.wav");
+	mario_die = loadSound ("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',got_results);
+}
+
+function modelLoaded(){
+	console.log ("poseNet is Initialized");
+}
+
+function got_results(result){
+     if(result.length > 0){
+		 console.log (result);
+		 noseX= result[0].pose.nose.x;
+		 noseY= result[0].pose.nose.y;
+	 }
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
